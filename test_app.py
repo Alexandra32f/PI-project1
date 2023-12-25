c060579c6182884e82deb0e7db465f8f03be96dc
Alexandra32f <135163308+Alexandra32f@users.noreply.github.com>
2023-12-25 21:57:58 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,8 +7,8 @@ Created on Mon Dec 11 19:07:31 2023
 
 
 from fastapi.testclient import TestClient
-from api import app
-from api import Item
+from fastapi import HTTPException
+from app import app, Item
 
 client = TestClient(app)
 
@@ -16,17 +16,37 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, World!"}
-    
-# def test_predict_positive():
-#     response = client.post("/predict/",
-#                            json={"text": "I like machine learning!"})
-#     json_data = response.json()
-#     assert response.status_code == 200
-#     assert json_data['label'] == 'score'
 
 def test_predict():
     item = Item(context="The Universe contains everything that exists", question="What is at the center of the solar system?")
     response = client.post("/predict/", json=item.dict())
+    
     assert response.status_code == 200
     assert "answer" in response.json()
-    assert "score" in response.json()    
+    assert "score" in response.json()
+
+def test_predict_invalid_input():
+    # Test case with missing context
+    invalid_item = {"question": "What is at the center of the solar system?"}
+    response = client.post("/predict/", json=invalid_item)
+    assert response.status_code == 422  # HTTP 422 Unprocessable Entity (validation error)
+
+    # Test case with missing question
+    invalid_item = {"context": "The Universe contains everything that exists"}
+    response = client.post("/predict/", json=invalid_item)
+    assert response.status_code == 422
+
+    # Test case with empty payload
+    empty_item = {}
+    response = client.post("/predict/", json=empty_item)
+    assert response.status_code == 422
+
+def test_predict_exception():
+    # Test case where the question-answering model raises an exception
+    def mock_classifier(*args, **kwargs):
+        raise Exception("Mocked exception")
+
+    app.classifier = mock_classifier
+    item = Item(context="The Universe contains everything that exists", question="What is at the center of the solar system?")
+    response = client.post("/predict/", json=item.dict())
+    assert response.status_code == 500  # HTTP 500 Internal Server Error
